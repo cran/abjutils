7f7d39ac27a53469e36e4307a9542643152342dc
Caio Lente <ctlente@gmail.com>
2018-01-19 17:26:56 +0000

    version 0.2.1

---
+++ R/cnj.R
@@ -0,0 +1,186 @@
+#' @title Calculate digits for Brazilian lawsuit identification numbers
+#' 
+#' @description Returns the check digit of a lawsuit numbers in the format 
+#' unified by the brazillian National Council of Justice.
+#' 
+#' @param num Ordered digits of the lawsuit number (including 0's) excluding
+#' the check digit
+#' @param build Whether or not the function return the complete lawsuit
+#' number (or only the check digits)?
+#'   
+#' @return The check digits or the complete identification number
+#'   
+#' @examples {
+#' calc_dig("001040620018260004", build = TRUE)
+#' calc_dig("001040620018260004", build = FALSE)
+#' 
+#' #will fail
+#' \dontrun{
+#' calc_dig("00104062001826000", build = TRUE)
+#' }
+#' }
+#' 
+#' @export
+calc_dig <- function(num, build = FALSE) {
+  
+  lengths <- stringr::str_length(num)
+  
+  if(max(lengths) != 18 | min(lengths) != 18){
+    stop("Lawsuit IDs without check digits should have 18 numerical digits.")
+  }
+  
+  NNNNNNN <- substr(num,  1L,  7L)
+  AAAA <-    substr(num,  8L, 11L)
+  JTR <-     substr(num, 12L, 14L)
+  OOOO <-    substr(num, 15L, 18L)
+  n1 <- sprintf('%02d', as.numeric(NNNNNNN) %% 97)
+  n2 <- sprintf('%02d', as.numeric(sprintf('%s%s%s', n1, AAAA, JTR)) %% 97)
+  n3 <- sprintf('%02d', 98 - ((as.numeric(sprintf('%s%s', n2, OOOO)) * 100) %% 97))
+  dig <- n3
+  if(build) {
+    return(sprintf('%s%s%s', substr(num, 1, 7), dig, substr(num, 8, 18)))
+  }
+  return(dig)
+}
+
+#' @title Validate check digits for Brazilian lawsuits identification
+#' number
+#' 
+#' @description Verifies if a check digit is correct
+#' 
+#' @param num String containing the complete lawsuit number
+#' 
+#' @return Whether or not the check digit is well calculated
+#'   
+#' @examples {
+#' check_dig("0005268-75.2013.8.26.0100")
+#' 
+#' \dontrun{
+#' check_dig("0005268-75.2013.8.26.100", build = TRUE)
+#' }
+#' }
+#' 
+#' @export
+check_dig <- function(num) {
+  
+  num <- stringr::str_replace_all(num, "[.-]","")
+  
+  if(stringr::str_length(num) != 20){
+    stop("Complete docket numbers should have 20 numerical digits.")
+  }
+  
+  num_no_dig <- stringr::str_c(substr(num,1L,7L),substr(num, 10L, 20L))
+
+  num_with_dig <- calc_dig(num_no_dig, build = TRUE)
+  
+  return(identical(num_with_dig, num))
+}
+
+
+#' @title Extract different parts from lawsuit ID
+#' 
+#' @description Given one or more lawsuit IDs, this function extracts one or more
+#' parts of the IDs given the following correspondence:
+#' \itemize{
+#'   \item "N": number
+#'   \item "D": verification digits
+#'   \item "A": year
+#'   \item "J": segment
+#'   \item "T": court
+#'   \item "O": origin
+#'   \item "": all of the above
+#' }
+#' 
+#' @param id One or more lawsuit IDs
+#' @param parts String or string vector with desired parts (see **description**)
+#' 
+#' @examples {
+#' extract_parts("001040620018260004", "N")
+#' extract_parts("001040620018260004", c("N", "A", "O"))
+#' }
+#' 
+#' @export
+extract_parts <- function(id, parts = "") {
+  
+  # Handle parts
+  parts <- unique(parts)
+  if (any(parts == "")) { parts <- c("N", "D", "A", "J", "T", "O") }
+  if (any(!(parts %in% c("N", "D", "A", "J", "T", "O")))) {
+    stop("Invalid parts")
+  }
+  
+  # Add verification digits to IDs that need them
+  id <- id %>%
+    clean_id() %>%
+    purrr::modify_if(~stringr::str_length(.x) == 18, calc_dig, build = TRUE) %>%
+    purrr::flatten_chr()
+  
+  # Extract parts for one ID
+  get_parts <- function(id, parts) {
+    
+    # For every part, run str_sub()
+    out <- c()
+    for (part in parts) {
+      
+      # Get range for str_sub()
+      range <- switch (part,
+        "N" = list(1, 7),
+        "D" = list(8, 9),
+        "A" = list(10, 13),
+        "J" = list(14, 14),
+        "T" = list(15, 16),
+        "O" = list(17, 20))
+      
+      # Add new element to list
+      out <- c(out, purrr::set_names(
+        stringr::str_sub(id, range[[1]], range[[2]]), part))
+    }
+    
+    return(out)
+  }
+
+  # Handle vectorization
+  purrr::map(id, get_parts, parts)
+}
+
+#' Remove separators from lawsuit IDs
+#' 
+#' @param id One or more lawsuit IDs
+#' 
+#' @export
+clean_id <- function(id) {
+  stringr::str_replace_all(id, pattern = "[\\-\\.]", replacement = "")
+}
+
+#' Add separators to lawsuit IDs
+#' 
+#' @param id One or more lawsuit IDs
+#' 
+#' @export
+build_id <- function(id) {
+  
+  # Build one ID
+  build <- function(id) {
+    stringr::str_c(id[1], "-", id[2], ".", id[3], ".",
+                   id[4], ".", id[5], ".", id[6])
+  }
+  
+  # Handle vectorization
+  purrr::map_chr(extract_parts(id), build)
+}
+
+#' @title Separate a lawsuit ID column into its parts
+#' 
+#' @description Wrapper around [tidyr::separate()] that splits a column
+#' with lawsuit IDs into 6 columns with its parts (see [extract_parts()]).
+#' Note that the IDs must be built (see [build_id()]).
+#' 
+#' @param data A data frame
+#' @param col Column name or position (see [tidyr::separate()])
+#' @param ... Other arguments passed on to [tidyr::separate()]
+#' 
+#' @export
+separate_cnj <- function(data, col, ...) {
+  tidyr::separate(
+    data, col, into = c("N", "D", "A", "J", "T", "O"), sep = "[\\-\\.]", ...)
+}
--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,21 @@
-#' Vectorize functions
+#' @title Vectorize functions (DEPRECATED)
 #'
-#' Iterate a function and wrap a dplyr::failtwith around it.
+#' @description Iterate a function and wrap a [dplyr::failwith()] around it.
 #'
 #' @importFrom magrittr %>%
 #'
-#' @param fun function to be iterated.
-#' @param itens character vector of inputs.
-#' @param ... outros other parameters for \code{fun}
-#' @param verbose should dvec print the current "item"?
-#' if \code{TRUE} (default) shows a message with probability p.
-#' @param p probability of printing a message. Only meaningful when verbose is 
-#'   \code{TRUE}.
+#' @param fun Function to be iterated
+#' @param itens Character vector of inputs
+#' @param ... Other parameters for `fun`
+#' @param verbose Should dvec print the current `item` (if `TRUE`, shows a
+#' message with probability `p`)?
+#' @param p Probability of printing a message
 #' 
 #' @export
 dvec <- function(fun, itens, ..., verbose = TRUE, p = .05) {
+  
+  warning("`dvec()` is deprecated; please use `abjutils::carefully()` instead.", call. = FALSE)
+  
   f <- dplyr::failwith(tibble::data_frame(result = 'erro'), fun)
   tibble::data_frame(item = itens) %>%
     dplyr::distinct(item) %>%
@@ -27,46 +29,107 @@ dvec <- function(fun, itens, ..., verbose = TRUE, p = .05) {
     dplyr::ungroup()
 }
 
-#' Remove accentuation
+#' @title Vectorized, parallel, safe and verbose function factory
+#'
+#' @description Wraps a function so that iterating over a set of inputs is
+#' easily parallelizable, and interruption-free.
+#'
+#' @param .f Function to be wrapped
+#' @param p Probability of function printing the index of the input it's
+#' currently processing
+#' @param cores Number of cores to use when iterating over vectorized
+#' inputs
 #' 
-#' Remove accented characters from strings converting them to ASCII.
+#' @examples
+#' \dontrun{
+#' # Function that takes a string and pastes two other strings
+#' # a its beginning and end respectivelly
+#' pad <- function(str, b = "", a = "") { paste0(b, str, a) }
+#' 
+#' # Create wrapped version of pad() that executes over 4 cores,
+#' # captures errors, and prints its current iteration with a
+#' # probability of 50%
+#' new_pad <- carefully(pad, p = 0.5, cores = 4)
+#' 
+#' # Execute new_pad() with some sample data
+#' new_pad(c("asdf", "poiu", "qwer"), b = "0", a = "1")
+#' }
+#' 
+#' @export
+carefully <- function(.f, p = 0.05, cores = 1) {
+  
+  # New, wrapped function
+  function(.x, ...) {
+    
+    # Wrap function
+    .f <- purrr::lift_dl(.f, ...)
+    .f_ <- purrr::possibly(.f, tibble::tibble(result = "ERROR"))
+    
+    # Create "closure" that carries .f_ and p
+    .c <- function(x, i) {
+      
+      # Print current index and apply function
+      if (runif(1) < p) { print(i) }
+      d <- .f_(x)
+      
+      # Convert into tibble if necessary
+      if (!is.data.frame(d)) { d <- dplyr::tibble(output = d) }
+      if (!tibble::has_name(d, "result")) { d$result <- "OK" }
+      
+      return(d)
+    }
+    
+    # Create cluster (and prepare to free it)
+    cl <- parallel::makeCluster(cores, outfile = "")
+    on.exit(parallel::stopCluster(cl))
+    
+    # Run function in parallel
+    out <- parallel::clusterMap(cl, .c, x = .x, i = seq_along(.x))
+    
+    # Bind rows and add input column
+    out <- dplyr::bind_rows(out)
+    out <- dplyr::bind_cols(tibble::tibble(input = .x), out)
+    
+    return(out)
+  }
+}
+
+#' @title Remove accentuation
+#' 
+#' @description Remove accented characters from strings converting them to
+#' ASCII.
 #' 
 #' @param x A string vector
 #' 
-#' @return A version of x without non-ASCII characters.
+#' @return A version of `x` without non-ASCII characters
 #' 
 #' @export
 rm_accent <- function(x) {
   if (.Platform$OS.type == 'unix') {
-    gsub("`", "", iconv(x, to = "ASCII//TRANSLIT"))
+    stringr::str_replace_all(iconv(x, to = "ASCII//TRANSLIT"), "[`'\"^~]", "")
   } else {
     gsub("`", "", iconv(x, from = 'latin1', to="ASCII//TRANSLIT"))
   }
 }
 
-#'Improved list of objects
+#' @title Improved list of objects
 #'
-#'Elegantly list objects in a R session.
+#' @description Elegantly list objects in a R session.
 #'
-#'@param pos where to look for the object (see "Details" in base::get 
-#'  documentation)
-#'@param pattern	an optional regular expression. Only names matching pattern are
-#'  returned. glob2rx can be used to convert wildcard patterns to regular 
-#'  expressions.
-#'@param order.by how should the list objects be sorted? Assume one of the 
-#'  following values:  "Type", "Size" (default), "Rows" or "Columns".
-#'@param decreasing should the sorting be decreasing? Skippable parameter.
-#'@param head should the "head" function be used for printing? TRUE by default.
-#'@param n how many lines the "head" function should show? (only meaningful when
-#'  head = TRUE) 10 by default.
+#' @param pos Where to look for the object (see "Details" in [base::get()]'s
+#' documentation)
+#' @param pattern	An optional regular expression to match names ([utils::glob2rx()]
+#' can be used to convert wildcard patterns to regular expressions)
+#' @param order.by Sort by `"Size"` (default), `"Type"`, `"Rows"` or `"Columns"`
+#' @param decreasing Should the sorting be decreasing?
+#' @param head Should [utils::head()] function be used for printing?
+#' @param n How many lines [utils::head()] function should show?
 #'  
-#'@section Credit:
-#' Taken from http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
-#'  
+#' @references http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
 #'  
 #'@export
 lsos <- function (pos = 1, pattern, order.by = "Size",
-                         decreasing=TRUE, head=TRUE, n=10) {
+                  decreasing=TRUE, head=TRUE, n=10) {
   napply <- function(names, fn) sapply(names, function(x)
     fn(get(x, pos = pos)))
   names <- ls(pos = pos, pattern = pattern)
@@ -86,3 +149,111 @@ lsos <- function (pos = 1, pattern, order.by = "Size",
     out <- head(out, n)
   out
 }
+
+#' @title Add pipe template
+#' 
+#' @description Adds pipe template to package documentation.
+#' 
+#' @param pkg Package description (can be path or package name)
+#' 
+#' @export
+use_pipe <- function(pkg = '.') {
+  pkg <- devtools::as.package(pkg)
+  devtools::use_package('magrittr', pkg = pkg)
+  txt_pipe <- readLines(system.file('pipe-op.R', 
+                                    package = 'abjutils'))
+  cat(txt_pipe, file = paste0(pkg$path, '/R/utils.R'),
+      append = TRUE, sep = '\n')
+  devtools::document()
+}
+
+#' Mirror of scales:::precision()
+#' 
+#' @param x See scales:::precision()
+#' 
+#' @export
+precision <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  span <- if (scales::zero_range(rng)) 
+    abs(rng[1])
+  else diff(rng)
+  if (span == 0) 
+    return(1)
+  10^floor(log10(span))
+}
+ 
+# #' @title Add a progress bar to any mapping function
+# #' 
+# #' @description This function wraps any function of the [purrr::map()] family,
+# #' adding a progress bar to the iteration.
+# #' 
+# #' @param ..f A function, formula, or atomic vector (see [purrr::map()])
+# #' 
+# #' @examples { \dontrun {
+# #' map_progress <- with_progress(purrr::map)
+# #' map_progress(1:10, ~{ Sys.sleep(1); .x })
+# #' }
+# #' 
+# #' @export
+# with_progress <- function(..f) {
+#   function(x, .f, ...) {
+#     .f <- purrr::as_mapper(.f)
+#     p <- progress::progress_bar$new(total = length(x))
+#     fun <- function(.x, ...) { p$tick(); .f(.x, ...) }
+#     ..f(x, fun, ...)
+#   }
+# }
+#
+# #' @title Add [purrr::possibly()] to any mapping function
+# #' 
+# #' @description This function wraps any function of the [purrr::map()] family,
+# #' capturing side effects with [purrr::possibly()]. This is useful when only
+# #' the side effects are desired as the returned values will be either `TRUE`
+# #' or `FALSE` (if that iteration did/didn't work).
+# #' 
+# #' @param ..f A function, formula, or atomic vector (see [purrr::map()])
+# #' 
+# #' @export
+# with_possibly <- function(..f) {
+#   function(x, .f, ...) {
+#     .f <- purrr::as_mapper(.f, ...)
+#     .f_ <- purrr::possibly(function(.x, ...) { .f(.x, ...); TRUE }, FALSE, FALSE)
+#     purrr::flatten_lgl(..f(x, .f_, ...))
+#   }
+# }
+# 
+# #' @title Add parallelism to any mapping function
+# #' 
+# #' @description This function wraps any function of the [purrr::map()] family,
+# #' parallelizing its iterations with [parallel::mcmapply()]. Note that flattening
+# #' doesn't work, so `purrr::map_***()` becomes only `purrr::map()`.
+# #' 
+# #' @param ..f A function, formula, or atomic vector (see [purrr::map()])
+# #' 
+# #' @export
+# with_parallelism <- function(..f) {
+#   function(x, .f, ...) {
+#     .f <- purrr::as_mapper(.f, ...)
+#     .f_ <- purrr::partial(..f, .f = .f)
+#     purrr::flatten(
+#       parallel::mcmapply(.f_, x, MoreArgs = ..., SIMPLIFY = FALSE))
+#   }
+# }
+# 
+# #' @title Add paralellism and [purrr::possibly()] to any mapping function
+# #' 
+# #' @description This function wraps any function of the [purrr::map()] family,
+# #' running everything in parallel and capturing side effects with [purrr::possibly()].
+# #' This is useful when only the side effects are desired as the returned values
+# #' will be either `TRUE` or `FALSE` (if that iteration did/didn't work).
+# #' 
+# #' @param ..f A function, formula, or atomic vector (see [purrr::map()])
+# #' 
+# #' @export
+# with_2p <- function(..f) {
+#   with_parallelism(with_possibly(..f))
+# }
+
+# Get rid of NOTEs
+globalVariables(c(
+  ".","item","object.size","%>%", "n_processo", "runif", "serial", "no_cd_code", "warn"))
